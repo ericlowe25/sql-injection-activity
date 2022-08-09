861d0301ab0bc999d15c7cd3b61d0fe29ba03f27
Eric Lowe <ericlowe25@gmail.ocm>
2022-08-08 22:57:23 -0400

    updated index.html file

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
-
-const http = require('http'),
-    path = require('path'),
-    express = require('express'),
-    bodyParser = require('body-parser');
-
 const sqlite3 = require('sqlite3').verbose();
+const http = require('http'),
+ path = require('path'),
+ express = require('express'),
+ bodyParser = require('body-parser');
+
 
 const app = express();
 app.use(express.static('.'))
-app.use(bodyParser.urlencoded({extend: true})) 
+app.use(bodyParser.urlencoded({extended: true})) 
 app.use(bodyParser.json())
 
 const db = new sqlite3.Database(':memory:');
    db.serialize(function () {
-    db.run("CREAT TABLE user (username TEXT, password TEXT, title TEXT)");
-    db.run("INSERT INTO user VALUES ('privilegedUser', 'privilegedUser1', 'Administrator')")
-   })
+    db.run("CREATE TABLE user (username TEXT, password TEXT, title TEXT)");
+    db.run("INSERT INTO user VALUES ('privilegedUser', 'privilegedUser1', 'Administrator')");
+   });
     
 app.get('/', function (req, res){
-    res.sendFile('index.html')
-})
+    res.sendFile('index.html');
+});
 
 app.post('/login', function (req,res) {
     var username = req.body.username
     var password = req.body.password
-    var query = "SELECT title FROM user where username = '"+ username +"'and password ='"+ password + "'"
+    var query = "SELECT title FROM user where username = '"+ username +"'and password ='"+ password + "'";
 console.log("username:" + username)
 console.log("password:" + password)
 console.log('query:' + query)
