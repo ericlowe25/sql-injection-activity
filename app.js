98187b20a2ee1fef9deb12804b566943b45f4f1f
Eric Lowe <ericlowe25@gmail.ocm>
2022-08-08 23:13:03 -0400

    finished

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const sqlite3 = require('sqlite3').verbose();
+
 const http = require('http'),
  path = require('path'),
  express = require('express'),
  bodyParser = require('body-parser');
-
+ const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
 app.use(express.static('.'))
